05b57dc77fdd34e9486ca1ecc07e898aea5258a3
Salvatore Dininni <salvatoredininni@Salvatores-MBP.attlocal.net>
2022-01-02 13:47:24 -0800

    game detail page implementations

--- frontend/src/Components/Views/Game/Game.js
+++ frontend/src/Components/Views/Game/Game.js
@@ -9,7 +9,7 @@ export default function Game() {
      let specificGame = `https://api.rawg.io/api/games/${slug}?&key=${apiKey}`;
 
      const [game, setGame]= useState(null);
-
+     
      useEffect(() => {
         fetch(specificGame)
            .then(resp => resp.json())
@@ -20,9 +20,34 @@ export default function Game() {
           <>
            {
                 game && 
-                    <div className="container-fluid   pt-5 min-vh-100" style={{background: `linear-gradient(rgba(33, 33, 33, 1), rgba(33, 33, 33, 0.8), rgba(33, 33, 33, 1)),url(${game.background_image})`}}>
+                    <div className="container-fluid   pt-5 min-vh-100" style={{background: `linear-gradient(rgba(33, 33, 33, 1), rgba(33, 33, 33, 0.8), rgba(33, 33, 33, 1)),url(${game.background_image})`,
+                      backgroundSize: 'cover',
+                      backbroudPosition: 'center',
+                      backgroundRepeat: 'no-repeat'
+                    }}>
                         <div className="container">
-                             Informations about the game
+                             <div className="row mt-5">
+                                 <div className="col-12">
+                                      <h1>{game.name}</h1>
+                                      <p className='small'> {game.developers[0].name}</p>
+                                 </div>
+                             </div>
+                             <div className="row mt-5">
+                                 <div className="col-12 col-md-6">
+                                     {game.description_raw} 
+                                 </div> 
+                                 <div className="col-12 col-md-6"> 
+                                        <img className='img-fluid' src={game.background_image} alt={game.name}/>  
+                                 </div>
+                                 <div className="row">
+                                      <h3>Genres</h3>
+                                      {
+                                        game.genres && game.genres.map(el => 
+                                             <span key={el.id}>{el.name}</span>)
+                                      }
+                                 </div>
+      
+                             </div>
                         </div>
                     </div>
            }
