53015c5544bea5e13713c763061aaaeba6cc913a
Salvatore Dininni <salvatoredininni@Salvatores-MBP.attlocal.net>
2022-01-04 14:47:06 -0800

    login and register requests implementations

--- frontend/src/Components/UI/SignIn/SignIn.js
+++ frontend/src/Components/UI/SignIn/SignIn.js
@@ -1,26 +1,72 @@
+import useInput from '../../../Hooks/useInput';
 
-export default function SignIn(params) {
-   
+import { AuthContext } from '../../../Contexts/Auth';
+import { ConfigContext } from '../../../Contexts/Config';
+
+import { useContext, useState } from 'react';
+
+export default function SignIn() {
+    const username = useInput('')
+    const email = useInput('')
+    const password = useInput('')
+    const passwordConfirm = useInput('')
+    
+    let { api_urls } = useContext(ConfigContext)
+    let { login } = useContext(AuthContext)
+
+    const singIn = (event) => {
+
+        event.preventDefault()
+
+        if (password.value === passwordConfirm.value) {
+              // proseguire 
+              //fetch register
+              //fetch login
+              //fetch view-profile
+
+                fetch(`${api_urls.backend}/api/users/register`, {
+                    method: 'POST',
+                    headers: {"Content-Type" : "application/json"},
+                    body: JSON.stringify({
+                        name: username.value, 
+                        email: email.value,
+                        password: password.value,
+                        passwordConfirm: passwordConfirm.value,
+                    })
+                }).then(response => {
+
+                    if (response.ok) {
+                        return response.json()
+                    } else {
+                        alert('ops..')
+                    }
+                })
+            
+        } else {
+              alert('the passwords are not the same')
+        }
+    }
+
     return (
         <>
-            <form className={`${'sign-form'}`}>
+            <form className={`${'sign-form'}`} onSubmit={signIn}>
                 <div className={`${'sign-top'}`}></div>
                 <div className={`${'sign-bottom'}`}></div>
                 <div className="mb-5">
                     <label className="form-label" htmlFor='userName'>Enter your User Name</label>
-                    <input type="text" className="form-control bg-transparent border-0 border-bottom border-info rounded-0" id="userName"/>
+                    <input type="text" className="form-control bg-transparent border-0 border-bottom border-info rounded-0" id="userName" {...username}/>
                 </div>
                 <div className="mb-5">
                     <label className="form-label" htmlFor='userMail'>Enter your Email</label>
-                    <input type="email" className="form-control bg-transparent border-0 border-bottom border-info rounded-0" id="userMail"/>
+                    <input type="email" className="form-control bg-transparent border-0 border-bottom border-info rounded-0" id="userMail" {...email}/>
                 </div>
                 <div className="mb-5">
                     <label className="form-label" htmlFor='userPassword'>Enter a new Password</label>
-                    <input type="password" className="form-control bg-transparent border-0 border-bottom border-info rounded-0" id="userPassword"/>
+                    <input type="password" className="form-control bg-transparent border-0 border-bottom border-info rounded-0" id="userPassword" {...password}/>
                 </div>
                 <div className="mb-5">
                     <label className="form-label" htmlFor='userPasswordConfirm'>Confirm the entered Password</label>
-                    <input type="password" className="form-control bg-transparent border-0 border-bottom border-info rounded-0" id="userPasswordConfirm"/>
+                    <input type="password" className="form-control bg-transparent border-0 border-bottom border-info rounded-0" id="userPasswordConfirm" {...passwordConfirm}/>
                 </div>
                 <div className="mb-5">
                     <button type='submit' className="btn btn-outline-info px-5 rounded-0">Register</button>
--- frontend/src/Components/UI/SignUp/SignUp.js
+++ frontend/src/Components/UI/SignUp/SignUp.js
@@ -1,31 +1,59 @@
-import { AuthContext } from './../../../Contexts/Auth';
+import useInput from './../../../Hooks/useInput';
+import { AuthContext } from './../../../Contexts/Auth/index';
 import { useContext } from 'react';
+import { ConfigContext } from './../../../Contexts/Config';
 
 export default function SignUp() {
 
-    let { login } = useContext(AuthContext)
+    let { login } = useContext(AuthContext);
+    let { api_urls } = useContext(ConfigContext);
+    const email = useInput('');
+    const password = useInput('');
+
     const signUp = (event) => {
         event.preventDefault()
-        console.log('ok');
-        login("Salvo", 'que', 122)
+        
+        fetch(`${api_urls.backend}/api/users/login`, {
+            method: 'POST',
+            headers: {"Content-Type" : "application/json"},
+            body: JSON.stringify({email: email.value, password: password.value})
+        })
+          .then(response => response.json())
+          .then(data => {
+              const token = data.token
+
+              /// una volta ricevuto il token, possiamo richiedere inofrmazioni come username e email ad esempio
+              //alla rotta view profile
+          })
     }
+
     return (
         
-            <form className={`${'sign-form'}`} onSubmit={signUp}>
-                <div className={`${'sign-top'}`}></div>
-                <div className={`${'sign-bottom'}`}></div>
-                <div className="mb-5">
-                    <label className="form-label" htmlFor='userMail'> Inserisci la tua email</label>
-                    <input type="email" className="form-control bg-transparent border-0 border-bottom border-info rounded-0" id="userMail"/>
-                </div>
-                <div className="mb-5">
-                    <label className="form-label" htmlFor='userPassword'> Inserisci la tua password</label>
-                    <input type="password" className="form-control bg-transparent border-0 border-bottom border-info rounded-0" id="userPassword"/>
-                </div>
-                <div className="mb-5">
-                    <button type="submit" className="btn btn-outline-info px-5 rounded-0">Login</button>
-                </div>
-            </form>
+        <form className={`${'sign-form'}`} onSubmit={signUp}>
+            <div className={`${'sign-top'}`}></div>
+            <div className={`${'sign-bottom'}`}></div>
+            <div className="mb-5">
+                <label className="form-label" htmlFor='userMail'> Inserisci la tua email</label>
+                <input 
+                    type="email" 
+                    className="form-control bg-transparent border-0 border-bottom border-info rounded-0" 
+                    id="userMail"
+                    {...email}
+                />
+            </div>
+            <div className="mb-5">
+                <label className="form-label" htmlFor='userPassword'> Inserisci la tua password</label>
+                <input 
+                    type="password" 
+                    className="form-control bg-transparent border-0 border-bottom border-info rounded-0" 
+                    id="userPassword"
+                    {...password}
+                />
+            </div>
+            <div className="mb-5">
+                <button type="submit" className="btn btn-outline-info px-5 rounded-0">Login</button>
+            </div>
+        </form>
 
     )
 }
