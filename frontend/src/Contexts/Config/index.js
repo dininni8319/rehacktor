549d51cf6349e99e3b098188412234d99b0badc1
Salvatore Dininni <s.dininni@yahoo.com>
2023-04-16 13:45:21 +0200

    saving local changes

--- frontend/src/Contexts/Config/index.js
+++ frontend/src/Contexts/Config/index.js
@@ -1,20 +1,53 @@
-import { createContext } from "react";
-// import { url, secret } from './Help';
+// import { createContext } from "react";
+// // import { url, secret } from './Help';
+
+// export const ConfigContext = createContext();
+
+// export function ConfigProvider(props) {
+//   const api_urls = {
+//     backend: process.env.REACT_APP_API_URL,
+//     games: process.env.REACT_APP_GAMES_API_URL,
+//     // games: url,
+//   };
+
+//   const api_secrets = {
+//     games: process.env.REACT_APP_GAMES_SECRET,
+//     // game: secret,
+//   };
+
+//   return (
+//     <ConfigContext.Provider value={{ api_urls, api_secrets }}>
+//       {props.children}
+//     </ConfigContext.Provider>
+//   );
+// }
+
+import { createContext } from 'react';
 
 export const ConfigContext = createContext();
 
 export function ConfigProvider(props) {
+    
+  const { 
+    REACT_APP_API_URL,
+    REACT_APP_PROD,
+    NODE_ENV,
+    REACT_APP_GAMES_SECRET,
+    REACT_APP_GAMES_API_URL
+  } = process.env
+
   const api_urls = {
-    backend: process.env.REACT_APP_API_URL,
-    games: process.env.REACT_APP_GAMES_API_URL,
-    // games: url,
+    backend: 
+      NODE_ENV === 'development' ?
+        REACT_APP_API_URL :
+        REACT_APP_PROD,
+    games: REACT_APP_GAMES_API_URL
   };
 
   const api_secrets = {
-    games: process.env.REACT_APP_GAMES_SECRET,
-    // game: secret,
-  };
-
+      games: REACT_APP_GAMES_SECRET
+  }
+    
   return (
     <ConfigContext.Provider value={{ api_urls, api_secrets }}>
       {props.children}
